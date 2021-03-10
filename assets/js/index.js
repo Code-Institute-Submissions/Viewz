5208f2e09674baf210b52a96a7878f16de775299
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-03-10 18:19:30 +0100

    Create and assign the variables in the index.js file

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+// Create and assign variables
+const appContainer = document.getElementById("app-container");
+const form = document.getElementById("form");
+const viewDescription = document.getElementById("viewDescription");
+const inputName = document.getElementById("name");
+const inputDate = document.getElementById("date");
+const inputImg = document.getElementById("view-img");
+const cardContainer = document.querySelector(".cards");
+const submit = document.querySelector(".submit");
+const viewList = document.querySelector(".list-views");
+const addView = document.querySelector(".add-view");
+const closeBtn = document.getElementById("close-btn");
+const popUp = document.getElementById("popUp");
+const pagination_element = document.getElementById("pagination");
+const checkMark = document.querySelector(".fa-check");
+let current_page = 1;
+let rows = 4;
+let image;
+let mapEvent;
+let map;
+let views = [];
+let marker;
